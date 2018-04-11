881c3d882d64cf38b7c0a14fc8fc81a6f5cbec77
celio gomes <bob.mg@hotmail.com>
2018-04-11 08:45:50 -0300

    criado o metodo listar categoria

--- src/main/java/com/celio/cursoSpring/resources/CategoriaResources.java
+++ src/main/java/com/celio/cursoSpring/resources/CategoriaResources.java
@@ -1,16 +1,27 @@
 package com.celio.cursoSpring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.celio.cursoSpring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/Categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionado";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		
+		List<Categoria> lista = new ArrayList<>(); 
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
